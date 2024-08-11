38f6c20fdb7f33942a064a462d1d997db1eef74d
muhammad hanif ramadhani <mhaniframadhani985@gmail.com>
2024-08-11 14:02:12 +0700

    chore: initialize project

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cecopy",
+    version="0.0.1",
+    author="muhammad hanif ramadhani",
+    author_email="mhaniframadhani985@gmail.com",
+    url="https://github.com/haniframadhani/cecopy",
+    packages=find_packages(exclude=['test*']),
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    python_requires=">=3.7"
+)
