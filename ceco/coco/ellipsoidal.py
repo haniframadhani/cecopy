476bed82b612c7ad5cdd353a26b08f2cbd9726b3
muhammad hanif ramadhani <mhaniframadhani985@gmail.com>
2025-03-10 11:00:23 +0700

    feat(ellipsoidal): add high conditioning option with Gram-Schmidt transformation

--- ceco/coco/ellipsoidal.py
+++ ceco/coco/ellipsoidal.py
@@ -18,14 +18,16 @@ class Ellipsoidal(Bbob):
     Attributes:
         x_opt (np.ndarray): The optimal shift vector, randomly generated within [-5, 5].
         f_opt (float): The function value at x_opt.
+        high_conditioning (bool): If `True`, applies an additional Gram-Schmidt transformation to introduce high conditioning.
     """
 
-    def __init__(self, dimension: int) -> None:
+    def __init__(self, dimension: int, high_conditioning: bool = None) -> None:
         """
         Initializes the Ellipsoidal function with a given dimension.
 
         Parameters:
             dimension (int): The number of dimensions for the input space. Must be a positive integer.
+            high_conditioning (bool, optional): If `True`, applies a Gram-Schmidt transformation to increase problem conditioning. Defaults to `False`.
 
         Raises:
             ValueError: If dimension is not a positive integer.
@@ -37,6 +39,8 @@ class Ellipsoidal(Bbob):
         self.x_opt = np.random.uniform(-5, 5, dimension)
 
         self.f_opt = self.raw(self.x_opt)
+
+        self.high_conditioning = high_conditioning
 
     def raw(self, x: np.ndarray) -> float:
         """
@@ -91,7 +95,10 @@ class Ellipsoidal(Bbob):
         z = input_vector - self.x_opt
 
         # Apply T_osz transformation
-        z = self.T_osz(z)
+        if not self.high_conditioning:
+            z = self.T_osz(z)
+        else:
+            z = self.T_osz(self.gram_schmidt(np.array([z]).T).ravel())
 
         # Compute the raw Ellipsoidal function value
         result = self.raw(z) + self.f_opt
